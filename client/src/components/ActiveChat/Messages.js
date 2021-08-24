10cdf3eb2fbb6f214b0f87ad9c1e78b6c89f543f
Scott Smith <scott.sncsmith@gmail.com>
2021-08-24 13:12:32 -0700

    Sender bubble display of last message

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -5,14 +5,20 @@ import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
+  const firstUnreadIndex = messages.findIndex((msg) => !msg.hasRead);
+  const lastMessageIndex = messages.length - 1;
 
   return (
     <Box>
-      {messages.map((message) => {
+      {messages.map((message, index) => {
         const time = moment(message.createdAt).format("h:mm");
+        const lastRead =
+          index + 1 === firstUnreadIndex
+          ||
+          (firstUnreadIndex === -1 && index === lastMessageIndex);
 
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <SenderBubble key={message.id} text={message.text} time={time} otherUser={lastRead ? otherUser : null} />
         ) : (
           <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
         );
