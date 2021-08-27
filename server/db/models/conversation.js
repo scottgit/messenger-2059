09b22bf749ec33d79ffabab0c545ed93654e1df4
Scott Smith <scott.sncsmith@gmail.com>
2021-08-27 15:18:06 -0700

    Modified Conversation model and associations in index.js of models

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,8 +1,28 @@
-const { Op } = require("sequelize");
+const { Op, Sequelize } = require("sequelize");
 const db = require("../db");
 const Message = require("./message");
 
-const Conversation = db.define("conversation", {});
+const Conversation = db.define("conversation", {
+  /* title -- takes the place of the current method of displaying the "activeConversation" as just the name of the other individual; now "activeConversation" will be tracked by conversationId and the title will display in areas where a username was previously showing the chat */
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: [1, 50]
+    }
+  },
+  /* initiatorId -- set on creation to indicate who started this conversation */
+  initiatorId: {
+    type: Sequelize.Integer,
+    allowNull: false,
+  },
+  /* private -- set on creation to allow any other users to join the conversation if they desire (without a current member issuing an invite); once public, there is currently no way to make private (but could implement a group vote to "privatize" the conversation after the fact) */
+  private: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  }
+});
 
 // find conversation given two user Ids
 
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,12 +1,14 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const Participant = require("./participant");
 
+/* The Participant table is a join table between Users and Conversations to allow any number of Users to be involved in any number of conversations */
 // associations
-
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+User.hasMany(Participant);
+Conversation.hasMany(Participant);
+Participant.belongsTo(User);
+Participant.belongsTo(Conversation);
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
